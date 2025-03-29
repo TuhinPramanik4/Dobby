d53a932ead010be265977cc6e4922658e42aeb88
TuhinPramanik4 <tuhinp475@gmail.com>
2025-03-29 18:41:25 +0530

    Set up gemini api

--- Backend/server.js
+++ Backend/server.js
@@ -27,6 +27,9 @@ app.use(cors({
     allowedHeaders: "Content-Type,Authorization"
 }));
 
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_API);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -35,6 +38,31 @@ mongoose.connect(process.env.MONGO_URI, {
 
 
 // ✅ Redirect Routes
+
+app.post("/api/ask-gemini", async (req, res) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    try {
+        const { question } = req.body;
+        if (!question) return res.status(400).json({ error: "Question is required" });
+        const prompt1 = `${question} + " recommend only two medicine drug name, nothing more"`;
+        const result1 = await model.generateContent(prompt1);
+        const response1 = result1.response.text();
+        
+        const prompt2 = `${question} + " recommend this to do in this situation: resting or going for a walk daily or any other activity, nothing more"`;
+        const result2 = await model.generateContent(prompt2);
+        const response2 = result2.response.text(); 
+        
+        res.json({ response1, response2 });
+        
+    } catch (error) {
+        console.error("❌ Error generating response:", error);
+        res.status(500).json({ error: "Error fetching AI response" });
+    }
+});
+
 app.get("/", (req, res) => {
     res.send("hello");
 });
