42d2daf1369ca7214e3d058f925877c9bb63ef1b
soham-mondal-001 <amisoham765@gmail.com>
2025-03-29 16:33:40 +0530

    Update Landing_page.jsx

--- Frontend/src/Components/Landing_page.jsx
+++ Frontend/src/Components/Landing_page.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Landing_Page_Home from './Landing_Page_Home'
+import Landing_about from './Landing_about'
 function Landing_page() {
   return (
-    <div>Landing_page</div>
+    <>
+    <Landing_Page_Home/>
+    <Landing_about/>
+    </>
   )
 }
 
