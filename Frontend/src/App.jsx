cfd6c685137fb946804fce906b9fb6d86b2d93f8
TuhinPramanik4 <tuhinp475@gmail.com>
2025-03-30 00:32:55 +0530

    Add doctor dashboard

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Medicine_Reminder from './Components/Medicine_Reminder';
 import Setnumber from './Components/Setnumber'
 import All_Doc from './Components/All_Doc';
 import Doctor_Login from './Components/Doctor_Login';
+import Doc_Dashboard from './Components/Doc_Dashboard';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,6 +21,7 @@ function App() {
         <Route path="/setnumber" element={<Setnumber />} />
         <Route path="/alldoctor" element={<All_Doc/>}  />
         <Route path="/doclogin" element={<Doctor_Login/>} />
+        <Route path="/docdash" element={<Doc_Dashboard/>} />
       </Routes>
     </Router>
   )
---
+++ Frontend/src/Components/Doc_Dashboard.jsx
@@ -0,0 +1,9 @@
+    import React from 'react'
+    
+    function Doc_Dashboard() {
+      return (
+        <div>Doctor Dashboard</div>
+      )
+    }
+    
+    export default Doc_Dashboard
