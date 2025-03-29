32697f419a15d2243e4a27230d34c2bcd60d30f7
TuhinPramanik4 <tuhinp475@gmail.com>
2025-03-29 18:35:53 +0530

    Add middlewares

--- Backend/server.js
+++ Backend/server.js
@@ -11,11 +11,21 @@ import cors from "cors";
 import cron from 'node-cron';
 import twilio from 'twilio';
 
+dotenv.config();
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use(express.json());
 
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
+}));
 
 
 // ✅ Redirect Routes
